732aa4a404b349e193fcd9f2ef42aecf9b47ce31
kwishashah <kwisha.shah2004@gmail.com>
2025-03-28 09:21:04 +0530

    commit

--- src/components/App12.jsx
+++ src/components/App12.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { useState,useRef } from 'react'
+import React from "react";
+import { useState, useRef, useEffect } from "react";
 export default function App12() {
-    const [price,setPrice]=useState(0);
-    const previousPrice=useRef(0);
-    const handleSubmit=()=>{
-        previousPrice.current=price
+  const [price, setPrice] = useState(0);
+  const previousPrice = useRef(0);
+  const priceRef = useRef();
+  useEffect(() => {
+    if (previousPrice.current > price) {
+      priceRef.current.style.color = "red";
+    } else {
+      priceRef.current.style.color = "green";
     }
+    previousPrice.current = price;
+  }, [price]);
+
   return (
-    <div>App12
-        <h3>This is app12</h3>
-        <p>
-            <input type="number"  />
-            <button onClick={handleSubmit}>Submit</button>
-        </p>
-        {previousPrice.current}-{price}
+    <div>
+      <h3>This is App12</h3>
+      <p>
+        <input type="number" onChange={(e) => setPrice(e.target.value)}></input>
+      </p>
+      Previous Price:{previousPrice.current}
+      <h4 ref={priceRef}>Current Price:{price}</h4>
     </div>
-    
-  )
+  );
 }
